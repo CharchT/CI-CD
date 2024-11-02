e43c15f9aa6de12678c28dbf4fedd80b3b8f8cd5
max kats <43301646+CharchT@users.noreply.github.com>
2024-11-02 21:55:55 +0300

    Create __tests__.js

---
+++ __tests__.js
@@ -0,0 +1,40 @@
+{
+    "testCases": [
+        {
+            "input": [1, 2, 3, 4, 5],
+            "expected": 120
+        },
+        {
+            "input": [-10, -10, 1, 3, 2],
+            "expected": 300
+        },
+        {
+            "input": [1, 2, 3, 4],
+            "expected": 24
+        },
+        {
+            "input": [0, 0, 0, 0],
+            "expected": 0
+        },
+        {
+            "input": [5, 5, 5, 5],
+            "expected": 625
+        },
+        {
+            "input": [-1, -2, -3, -4],
+            "expected": -6
+        },
+        {
+            "input": [100, 200, 300, 400, 500],
+            "expected": 30000000
+        },
+        {
+            "input": [1, 2, 3],
+            "expected": "Error"
+        },
+        {
+            "input": [],
+            "expected": "Error"
+        }
+    ]
+}
